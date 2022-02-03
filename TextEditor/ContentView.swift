0cc65b77aa1682c356d0efec79ffacd845500563
Leonardo Lemos <leonardo@sbsistemas.com.br>
2022-02-03 19:42:25 -0300

    Initial Commit

---
+++ TextEditor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TextEditor
+//
+//  Created by Leonardo Lemos on 03/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TextEditor/TextEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextEditorApp.swift
+//  TextEditor
+//
+//  Created by Leonardo Lemos on 03/02/22.
+//
+
+import SwiftUI
+
+@main
+struct TextEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
