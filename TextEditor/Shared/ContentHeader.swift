900580ed9780d0d08d2b5cd945dbf541e2cc8ad8
Leonardo Lemos <leonardo@sbsistemas.com.br>
2022-02-03 21:46:27 -0300

    feat: add content header to contain a file pathname display

---
+++ TextEditor/Shared/ContentHeader.swift
@@ -0,0 +1,20 @@
+//
+//  ContentHeader.swift
+//  TextEditor
+//
+//  Created by Leonardo Lemos on 03/02/22.
+//
+
+import SwiftUI
+
+struct ContentHeader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentHeader_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentHeader()
+    }
+}
